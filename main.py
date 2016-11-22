fbce79aafd542137dbb2b1259bcd0069b30c41ff
Kyle Di Giorgio <kdigiorgio14@gmail.com>
2016-11-21 20:21:06 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -3,8 +3,6 @@ import graph_builder
 import tshark
 import time
 import argparse
-import thread
-import subprocess
 import os
 import glob
 
@@ -12,8 +10,9 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-w ', '--write ', help="Name of the new PCAP file to be created",\
                         action="store", dest="write_file")
-    parser.add_argument('-r ', '--read ', help="Reads a pcap file and build a visual"\
-                        " representation of it", action="store", dest="read_file")
+    parser.add_argument('-r ', '--read ', help="Give the absolute path to the\
+                        folder containing a pcap file(s). Reads the pcap file\
+                         and build a visual representation of it", action="store", dest="read_file")
     args = parser.parse_args()
 
     if args.write_file:
@@ -21,11 +20,19 @@ def main():
         tshark.tshark(args.write_file)
         time.sleep(60)   #temporary solution, need to implement threading
         graph_builder.build_graph(pcap_parser.complete_view(args.write_file), '%s_complete_view' % args.write_file[:-5])
+        graph_builder.build_graph(pcap_parser.complete_view(pcap_file), '%s_red_team' % os.path.basename(pcap_file[:-5]))
+        graph_builder.build_graph(pcap_parser.complete_view(pcap_file), '%s_scorebot' % os.path.basename(pcap_file[:-5]))
         graph_builder.build_graph(pcap_parser.file_transfer(args.write_file), '%s_file_transfer' % args.write_file[:-5])
     elif args.read_file:
+        #in order to provide a clear visualization I had to
+        #use editcap to cute the overall pcap files into smaller
+        #"bites". This functon reads each of the smaller pcap file_transfer
+        #and builds three graphs from them.
         for pcap_file in glob.glob(os.path.join(args.read_file, '*.pcap')):
-            graph_builder.build_graph(pcap_parser.complete_view(pcap_file), '%s_complete_view' % args.read_file[:-5])
-            graph_builder.build_graph(pcap_parser.file_transfer(pcap_file), '%s_file_transfer' % args.read_file[:-5])
+            #graph_builder.build_graph(pcap_parser.complete_view(pcap_file), '%s_complete_view' % os.path.basename(pcap_file[:-5]))
+            #graph_builder.build_graph(pcap_parser.complete_view(pcap_file), '%s_red_team' % os.path.basename(pcap_file[:-5]))
+            graph_builder.build_graph(pcap_parser.complete_view(pcap_file), '%s_scorebot' % os.path.basename(pcap_file[:-5]))
+            #graph_builder.build_graph(pcap_parser.file_transfer(pcap_file), '%s_file_transfer' % os.path.basename(pcap_file[:-5]))
 
 if __name__ == '__main__':
     main()
