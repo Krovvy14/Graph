ae9a004bfda9ea27848a75d592a1bdce9117e123
Kyle Di Giorgio <kdigiorgio14@gmail.com>
2016-11-26 15:19:38 -0500

    Add files via upload
    Have the ability to now read and create new pcap files which automatically generate graph information.

--- main.py
+++ main.py
@@ -1,28 +1,35 @@
 import pcap_parser
 import graph_builder
 import tshark
-import time
 import argparse
 import os
 import glob
 
+
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-w ', '--write ', help="Name of the new PCAP file to be created",\
-                        action="store", dest="write_file")
-    parser.add_argument('-r ', '--read ', help="Give the absolute path to the\
-                        folder containing a pcap file(s). Reads the pcap file\
-                         and build a visual representation of it", action="store", dest="read_file")
+    parser.add_argument('-w ', '--write ', help="Name of the new PCAP file to"
+                        "be created", action="store", dest="write_file")
+    parser.add_argument('-r ', '--read ', help="Give the absolute path to the"
+                        "folder containing a pcap file(s). Reads the pcap file"
+                        " and build a visual representation of it",
+                        action="store", dest="read_file")
     args = parser.parse_args()
 
     if args.write_file:
-        #thread.start_new_thread(tshark.tshark,(args.write_file, ))
-        tshark.tshark(args.write_file)
-        time.sleep(60)   #temporary solution, need to implement threading
-        graph_builder.build_graph(pcap_parser.complete_view(args.write_file), '%s_complete_view' % args.write_file[:-5])
-        #graph_builder.build_graph(pcap_parser.red_team(pcap_file), '%s_red_team' % os.path.basename(pcap_file[:-5]))
-        #graph_builder.build_graph(pcap_parser.scorebot(pcap_file), '%s_scorebot' % os.path.basename(pcap_file[:-5]))
-        #graph_builder.build_graph(pcap_parser.file_transfer(args.write_file), '%s_file_transfer' % args.write_file[:-5])
+        try:
+            tshark.traffic_capture(args.write_file)
+        except KeyboardInterrupt:
+            for pcap_file in os.listdir(args.write_file):
+                graph_builder.build_graph(pcap_parser.complete_view(pcap_file),
+                                          '%s_complete_view' % pcap_file)
+                graph_builder.build_graph(pcap_parser.red_team(pcap_file),
+                                          '%s_red_team' % pcap_file)
+                graph_builder.build_graph(pcap_parser.scorebot(pcap_file),
+                                          '%s_scorebot' % pcap_file)
+                graph_builder.build_graph(pcap_parser.file_transfer(pcap_file)
+                                          , '%s_file_transfer' % pcap_file)
+
     elif args.read_file:
         # in order to provide a clear visualization I had to
         # use editcap to cute the overall pcap files into smaller
--- tshark.py
+++ tshark.py
@@ -1,4 +1,6 @@
 import subprocess
+import os
+import signal
 
 '''
 Starts a tshark pacet capture and saves it to a file
@@ -6,8 +8,11 @@ that is passed as a functional parameter.  At the moment
 it captures 150 packets. That is what looks the most
 "clean" in my current graph building module.
 '''
-def tshark(filename):
-	try:
-		subprocess.Popen(['tshark', '-i', '1','-a','duration:60','-w','%s' % filename, '-F', 'pcap'])
-	except KeyboardInterrupt:
-		return
+def traffic_capture(out_directory):
+    if not os.path.exists(out_directory):
+        os.mkdir(out_directory)
+    os.chdir(out_directory)
+    process = subprocess.call(['tshark', '-i', '1', '-b', 'filesize:1000', '-b',
+                      'files:10000', '-w', 'capture'])
+    if signal.SIGINT:
+        os.kill(process.pid, signal.SIGINT)
