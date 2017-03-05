736f769e1ef33a9fbab4dd841b66b1b27245cd61
Kyle <kdigiorgio14@gmail.com>
2017-03-04 21:14:53 -0500

    renamed:    python/ingester.py -> ingester.py 	renamed:    python/ingester.pyc -> ingester.pyc 	modified:   main.py 	new file:   notes.txt 	renamed:    python/pcap2csv.py -> pcap2csv.py 	new file:   pcap2csv.pyc 	deleted:    python/main.py 	deleted:    python/pcap2csv.pyc 	deleted:    python/server.pyc 	renamed:    python/server.py -> server.py 	new file:   server.pyc 	renamed:    python/tshark.py -> tshark.py 	renamed:    python/tshark.pyc -> tshark.pyc 	modified:   webapp/index.html 	new file:   webapp/main_graph.js 	modified:   webapp/src/css/style.css 	modified:   webapp/src/js/main_graph.js

--- main.py
+++ main.py
@@ -12,45 +12,50 @@ import time
 from multiprocessing import Process
 
 
+PCAP_DIR = os.getcwd() + "/pcap"
+CSV_DIR = os.getcwd() + "/csv"
+WEBAPP_DIR = os.getcwd() + "/webapp/" # find a way to get webapp to start server
+
 def main():
 	parser = argparse.ArgumentParser()
 	parser.add_argument('-w ', '--write ', help="Name of the new PCAP file to"
-                        "be created. Will be stored as a csv for ingesting into"
+						"be created. Will be stored as a csv for ingesting into"
 						" neo4j database", action="store_true", dest="write_file")
 	parser.add_argument('-r ', '--read ', help="Give the absolute path to the"
-                        "folder containing a pcap file(s). Reads the pcap file"
-                        " and convert it to a csv. This is then ingested into"
+						"folder containing a pcap file(s). Reads the pcap file"
+						" and convert it to a csv. This is then ingested into"
 						"a neo4j database", action="store", dest="read_file")
 	args = parser.parse_args()
 
+	print "WEBAPP_DIR: %s" % WEBAPP_DIR
 	if args.write_file:
-		pcap_dir = "/tmp/pcap"
-		csv_dir = "/tmp/csv"
 		pcap_files = []
 		csv_files = []
 
-		if not os.path.exists(pcap_dir):
-			os.mkdir(pcap_dir)
-		if not os.path.exists(csv_dir):
-			os.mkdir(csv_dir)
+		if not os.path.exists(PCAP_DIR):
+			os.mkdir(PCAP_DIR)
+		if not os.path.exists(CSV_DIR):
+			os.mkdir(CSV_DIR)
+
 
 		try:
-			tshark_process = Process(target=tshark.traffic_capture, args=(pcap_dir,))
-			server_process = Process(target=server.server, args=(csv_dir,))	
+			tshark_process = Process(target=tshark.traffic_capture, args=(PCAP_DIR,))
+			server_process = Process(target=server.server, args=(CSV_DIR, 8080,))	
+			webapp_process = Process(target=server.server, args=(WEBAPP_DIR, 8000,))
 			server_process.start()
 			tshark_process.start()
-
+			webapp_process.start()
 			while 1:
 				time.sleep(30)
-				for pcap in os.listdir(pcap_dir):
+				for pcap in os.listdir(PCAP_DIR):
 					print "pcap filename: %s" % pcap
 					if not pcap in pcap_files:
 						pcap_files.append(pcap)
-						print "File being processed: %s" % pcap_dir + '/' + pcap
-						csv_process = Process(target=pcap2csv.parser, args=(pcap_dir + '/' + pcap,))
+						print "File being processed: %s" % PCAP_DIR + '/' + pcap
+						csv_process = Process(target=pcap2csv.parser, args=(PCAP_DIR + '/' + pcap,))
 						csv_process.start()
 						csv_process.join()
-				for csv in os.listdir(csv_dir):
+				for csv in os.listdir(CSV_DIR):
 					print "csv filename: %s" % csv
 					if not csv in csv_files:
 						csv_files.append(csv)
@@ -62,17 +67,20 @@ def main():
 			print "Keyboard interrupt detected"		
 			tshark_process.join()
 			server_process.join()
+			webapp_process.join()
 
 	elif args.read_file:
+
+		print "Webapp dir: %s" % WEBAPP_DIR
 		try:
-			csv_dir = '/tmp/csv'
+			if not os.path.exists(CSV_DIR):
+				os.mkdir(CSV_DIR)
 
-			if not os.path.exists(csv_dir):
-				os.mkdir(csv_dir)
+			server_process = Process(target=server.server, args=(CSV_DIR, 8080,))
 
-			server_process = Process(target=server.server, args=(csv_dir,))
-			server_process.start()
+			webapp_process = Process(target=server.server, args=(WEBAPP_DIR, 8000,))
 
+			webapp_process.start()
 			for pcap_file in os.listdir(args.read_file):
 				infile = args.read_file + '/' + pcap_file
 				pcap2csv.parser(infile)
@@ -81,7 +89,7 @@ def main():
 			server_process.join()
 		except KeyboardInterrupt:
 			print "Keyboard interrupt detected"
-	
+			webapp_process.join()	
 
 if __name__ == '__main__':
 	main()
